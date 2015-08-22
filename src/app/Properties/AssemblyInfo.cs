0b9b79396973f9269740453ea057e02894026ed1
Dreanor <mikeundead@googlemail.com>
2015-08-23 01:41:41 +0200

    exe test

--- src/app/Properties/AssemblyInfo.cs
+++ src/app/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("D55998FC-5472-4419-A226-48FF75EDFADC")]
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.0.6.1")]
+[assembly: AssemblyFileVersion("1.0.6.1")]

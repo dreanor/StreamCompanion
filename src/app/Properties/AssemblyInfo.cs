fa9a11afe9f8aed40003466587d580447e8d2bb2
Dreanor <mikeundead@googlemail.com>
2015-08-23 00:51:45 +0200

    deploy test

--- src/app/Properties/AssemblyInfo.cs
+++ src/app/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Windows;
     ResourceDictionaryLocation.None, 
     ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("1.0.6.2")]
-[assembly: AssemblyFileVersion("1.0.6.2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

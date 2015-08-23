f527f8b8ae2a8a8461b09a7fd3f70836a8fc4f8e
Dreanor <mikeundead@googlemail.com>
2015-08-23 17:50:16 +0200

    test

---
+++ src/Tests/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using StreamCompanion.Contract;
+using StreamCompanion.Contract.StreamTemplate;
+using StreamCompanion.Controller;
+using StreamCompanion.StreamTemplate;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestMeh() 
+        {
+            IStreamItem m = new StreamItem("a", "b");
+        }
+    }
+}

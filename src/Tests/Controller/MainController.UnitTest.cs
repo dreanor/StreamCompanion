19cdd2086c7f7e9b343a100f31fee66d668decda
Dreanor <mikeundead@googlemail.com>
2015-08-27 19:51:11 +0200

    Added RhinoMocks

---
+++ src/Tests/Controller/MainController.UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using StreamCompanion.Contract;
+using StreamCompanion.Contract.ShellBase.Model;
+using StreamCompanion.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamCompanion.Tests.Controller
+{
+    [TestFixture]
+    public class MainControllerUnitTest
+    {
+        [Test]
+        public void CanGenerateNextEpisodeStream() 
+        {
+            IStepModel stepModel = MockRepository.GenerateMock<IStepModel>();
+            IController mainController = new MainController(stepModel);
+        }
+    }
+}

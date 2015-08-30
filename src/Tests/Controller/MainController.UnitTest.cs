dcf12126c3d59ec95972ef09aa155b93b3106bb2
Dreanor <mikeundead@googlemail.com>
2015-08-30 23:07:28 +0200

    Completed #9

--- src/Tests/Controller/MainController.UnitTest.cs
+++ src/Tests/Controller/MainController.UnitTest.cs
@@ -3,22 +3,33 @@ using Rhino.Mocks;
 using StreamCompanion.Contract;
 using StreamCompanion.Contract.ShellBase.Model;
 using StreamCompanion.Controller;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using StreamCompanion.ItemViewModel;
 
 namespace StreamCompanion.Tests.Controller
 {
     [TestFixture]
     public class MainControllerUnitTest
     {
+        IController mainController;
+        [SetUp]
+        public void SetUp() 
+        {
+            IStepModel stepModelMock = MockRepository.GenerateMock<IStepModel>();
+            mainController = new MainController(stepModelMock);
+        }
+
         [Test]
         public void CanGenerateNextEpisodeStream() 
         {
-            IStepModel stepModel = MockRepository.GenerateMock<IStepModel>();
-            IController mainController = new MainController(stepModel);
+            const string expectedGeneratedStream = "https://www.google.com/#q=Test+stream&safe=off";
+            ISerie serieMock = MockRepository.GenerateMock<ISerie>();
+            serieMock.Stub(x => x.Title).Return("Test");
+            serieMock.Stub(x => x.Progress).Return(new Progress(0, 1, 1));
+            serieMock.Stub(x => x.Type).Return("Movie");
+
+            string actualGeneratedStream = mainController.GenerateNextEpisodeStream(serieMock);
+
+            Assert.AreEqual(expectedGeneratedStream, actualGeneratedStream);
         }
     }
 }

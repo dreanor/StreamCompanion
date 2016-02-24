fcc74ec3c91bf7711f6329312d48fca11aa56f6f
Dreanor <mikeundead@googlemail.com>
2016-02-25 00:22:09 +0100

    updated views

--- src/app/ShellView.xaml.cs
+++ src/app/ShellView.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using MahApps.Metro.Controls;
-using MahApps.Metro.Controls.Dialogs;
-using Octokit;
 using StreamCompanion.Contract.ShellBase.Shell;
-using System.Diagnostics;
 using Application = System.Windows.Forms.Application;
 
 namespace StreamCompanion.App
@@ -14,33 +11,32 @@ namespace StreamCompanion.App
             InitializeComponent();
             this.DataContext = viewModel;
             this.Title = string.Format("{0} - {1}", this.Title, Application.ProductVersion);
-            //CheckForNewVersion();
         }
 
-        private async void CheckForNewVersion()
-        {
-            GitHubClient client = new GitHubClient(new ProductHeaderValue("StreamCompanion"));
-            var user = client.User.Get("dreanor");
-            var releases = await client.Release.GetAll("dreanor", "StreamCompanion");
-            var latest = releases[0];
+        //private async void CheckForNewVersion()
+        //{
+        //    GitHubClient client = new GitHubClient(new ProductHeaderValue("StreamCompanion"));
+        //    var user = client.User.Get("dreanor");
+        //    var releases = await client.Release.GetAll("dreanor", "StreamCompanion");
+        //    var latest = releases[0];
 
-            if (!latest.TagName.Equals(Application.ProductVersion.ToString()))
-            {
-                string releaseUrl = "https://github.com/dreanor/StreamCompanion/releases/tag/" + latest.TagName;
+        //    if (!latest.TagName.Equals(Application.ProductVersion.ToString()))
+        //    {
+        //        string releaseUrl = "https://github.com/dreanor/StreamCompanion/releases/tag/" + latest.TagName;
 
-                this.MetroDialogOptions.ColorScheme = MetroDialogColorScheme.Theme;
-                this.MetroDialogOptions.NegativeButtonText = "Not now";
-                this.MetroDialogOptions.AffirmativeButtonText = "Visit GitHub";
+        //        this.MetroDialogOptions.ColorScheme = MetroDialogColorScheme.Theme;
+        //        this.MetroDialogOptions.NegativeButtonText = "Not now";
+        //        this.MetroDialogOptions.AffirmativeButtonText = "Visit GitHub";
 
-                var result = await this.ShowMessageAsync("New Version!", "A new release is available for download on GitHub.", 
-                    MessageDialogStyle.AffirmativeAndNegative, 
-                    this.MetroDialogOptions);
+        //        var result = await this.ShowMessageAsync("New Version!", "A new release is available for download on GitHub.", 
+        //            MessageDialogStyle.AffirmativeAndNegative, 
+        //            this.MetroDialogOptions);
 
-                if(result == MessageDialogResult.Affirmative)
-                {
-                    Process.Start(releaseUrl);
-                }
-            }
-        }
+        //        if(result == MessageDialogResult.Affirmative)
+        //        {
+        //            Process.Start(releaseUrl);
+        //        }
+        //    }
+        //}
     }
 }
---
+++ src/uic/StatisticTab/StatisticUic.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StreamCompanion.Uic
+{
+    /// <summary>
+    /// Interaction logic for StatisticUic.xaml
+    /// </summary>
+    public partial class StatisticUic : UserControl
+    {
+        public StatisticUic()
+        {
+            InitializeComponent();
+        }
+    }
+}

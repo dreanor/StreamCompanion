31985b266b848dcad7e4bcc032227a70aa7c7268
Dreanor <mikeundead@googlemail.com>
2016-04-24 02:22:16 +0200

    small project changes

--- src/app/ShellView.xaml.cs
+++ src/app/ShellView.xaml.cs
@@ -12,31 +12,5 @@ namespace StreamCompanion.App
             this.DataContext = viewModel;
             this.Title = string.Format("{0} - {1}", this.Title, Application.ProductVersion);
         }
-
-        //private async void CheckForNewVersion()
-        //{
-        //    GitHubClient client = new GitHubClient(new ProductHeaderValue("StreamCompanion"));
-        //    var user = client.User.Get("dreanor");
-        //    var releases = await client.Release.GetAll("dreanor", "StreamCompanion");
-        //    var latest = releases[0];
-
-        //    if (!latest.TagName.Equals(Application.ProductVersion.ToString()))
-        //    {
-        //        string releaseUrl = "https://github.com/dreanor/StreamCompanion/releases/tag/" + latest.TagName;
-
-        //        this.MetroDialogOptions.ColorScheme = MetroDialogColorScheme.Theme;
-        //        this.MetroDialogOptions.NegativeButtonText = "Not now";
-        //        this.MetroDialogOptions.AffirmativeButtonText = "Visit GitHub";
-
-        //        var result = await this.ShowMessageAsync("New Version!", "A new release is available for download on GitHub.", 
-        //            MessageDialogStyle.AffirmativeAndNegative, 
-        //            this.MetroDialogOptions);
-
-        //        if(result == MessageDialogResult.Affirmative)
-        //        {
-        //            Process.Start(releaseUrl);
-        //        }
-        //    }
-        //}
     }
 }
